93d24c39f351590aaca8c3138fd289f2e8a00331
Tomasz Badelek <tbadelek@gmail.com>
2017-11-04 20:05:28 +0100

    2nd

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -12,8 +12,9 @@ import javax.persistence.*;
 @Entity(name = "tasks")
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
 
     @Column(name = "name")
     private String title;
